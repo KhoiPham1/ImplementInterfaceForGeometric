e25bf337230ae7da68a5ae550dbd03796498a1dc
Khoi Pham <khoi171195@gmail.com>
2018-11-07 17:00:14 +0700

    Firstcommit

---
+++ src/Circle.java
@@ -0,0 +1,50 @@
+public class Circle {
+    private double radius = 1.0;
+    private String color;
+    private boolean filled = true;
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+        this.radius = radius;
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public String toString(){
+        return "Radius: "+ getRadius()
+                +" Color: "+ getColor()
+                +" Filled: "+ (isFilled()?"fill":"not fill") ;
+    }
+
+
+}
